1bf6c647ab38a0170b35448970f5635d1b3ff6e7
Ian Norton <i.d.norton@gmail.com>
2019-01-04 20:52:52 +0000

    Adding first cut terraform plan

---
+++ terraform/main.tf
@@ -0,0 +1,7 @@
+resource "scaleway_server" "interview" {
+  name  = "interview"
+  image = "c564be4f-2dac-4b1b-a239-3f3a441700ed"
+  type  = "START1-XS"
+  state = "running"
+  dynamic_ip_required = true
+}
