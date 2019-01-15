5bdec06b52231d71e10031186a33d54e05e48fb1
Ian Norton <iann@thermeon.com>
2019-01-15 10:30:51 +0000

    Genericise plan to be bootstrap rather than interview

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
-resource "scaleway_server" "interview" {
-  name  = "interview"
+resource "scaleway_server" "bootstrap" {
+  name  = "bootstrap"
   image = "c564be4f-2dac-4b1b-a239-3f3a441700ed"
   type  = "START1-XS"
   state = "running"
