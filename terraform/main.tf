345ab309c1632a9584b202897373fb69d79c5181
Ian Norton <ian.norton@matillion.com>
2020-05-14 14:25:18 +0100

    Update to Ubuntu 18.04

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,10 @@
 resource "scaleway_server" "bootstrap" {
   name  = "bootstrap"
-  image = "c564be4f-2dac-4b1b-a239-3f3a441700ed"
-  type  = "START1-XS"
+  image = "d4067cdc-dc9d-4810-8a26-0dae51d7df42" // Ubuntu 18.04 Bionic Paris1 x86_64
+  type  = "START1-S"
   state = "running"
   dynamic_ip_required = true
 }
+
+//  image = "c564be4f-2dac-4b1b-a239-3f3a441700ed" // Ubuntu 16.04 Xenial Paris1 x86_64
+//  This image is 25GB version
